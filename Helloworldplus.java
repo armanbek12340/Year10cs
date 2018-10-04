631521778b8929cb31abfb3a208c9dd70ead61f4
Armanbek Narzibekov <arman.narzibekov@Arman.local>
2018-10-04 09:23:55 -0400

    pushing my helloworld

---
+++ Helloworldplus.java
@@ -0,0 +1,9 @@
+public class Helloworldplus {
+	public static void main(String [] args){
+			for (int i=0; i < 10; i = i + 1) {
+				if (i == 9) {
+					System.out.println("hi" + " " + i);
+	}
+}
+}
+}
